54a15e02c39768fe9393b60e4926203dc121510b
Beaaaaa25 <bahaghari2508@gmail.com>
2024-09-26 00:49:01 +0800

    Add files via upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,29 @@
+@extends('base')
+
+@section('content')
+<div class="container">
+    <div class="card mt-5">
+        <div class="card-header bg-success">
+            <h3 class="card-title">HAHAHHAHA</h3>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+        </div>
+        <div class="card-body" style="display: block;">
+            
+        </div>
+
+        <div class="card-footer" style="display: block;">
+            
+        </div>
+
+    </div>
+
+    
+</div>
+@endsection
